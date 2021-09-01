5e4ed2c8386abf1045fb80e5d3d39e0d3eb91d02
Gowtham <56188220+Gowtham-GitHub@users.noreply.github.com>
2021-09-01 10:08:08 +0530

    updated code
    line 4, printf to printIn

--- Samplecode.java
+++ Samplecode.java
@@ -1,6 +1,6 @@
 class Simple{
   public static void main(String args[]) throws InterruptedException{
   Thread.sleep(5000);
-  System.out.printf("Hello World");
+  System.out.printIn("Hello World");
   }
 }
